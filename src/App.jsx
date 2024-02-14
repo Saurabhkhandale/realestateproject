476fdb028b09709f0f3da05f7cfec85c26f54c51
Saurabhkhandale <saurabhkhandale3991@gmail.com>
2024-02-14 14:51:56 +0530

    react js installed and tailwind installed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <p className="text-red-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro animi nam
+        consequuntur adipisci laudantium, reprehenderit atque blanditiis
+        consequatur eos commodi! Maxime omnis fuga suscipit praesentium iusto
+        aliquam rerum aspernatur, nemo assumenda ratione quae vel. Facere
+        beatae, unde eum officiis, laudantium earum ex cumque repellendus
+        perferendis ipsam ea doloremque perspiciatis nemo!
+      </p>
+    </div>
+  );
+}
