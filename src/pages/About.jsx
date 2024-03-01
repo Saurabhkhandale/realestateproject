949c09705d18dd66bda0060859b1d55d0d6602cd
Saurabhkhandale <saurabhkhandale3991@gmail.com>
2024-03-01 23:31:50 +0530

    added sample push

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ export default function About() {
   return (
     <div>
       <p>About</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam sapiente error quae nihil distinctio fuga doloremque velit voluptatum dolore, asperiores blanditiis, in placeat aliquam consequuntur odio earum laboriosam vitae magnam quaerat id ut fugiat veritatis? Ipsum voluptatem fugit sequi deserunt? Dolor voluptatem autem dicta illum. Corrupti facilis illo excepturi vitae.</p>
     </div>
   );
 }
